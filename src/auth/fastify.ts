9b083f894ad499f9e6a8da3ce91e9a1f2f00bec2
Alex Seifert <alex@alexseifert.com>
2023-01-04 22:27:43 +0100

    Stip the code down to the bare minimum for now

--- src/auth/fastify.ts
+++
@@ -1,9 +0,0 @@
-import fastifyPassport from '@fastify/passport';
-
-export const fastifyPreValidationJwt = {
-  preValidation: fastifyPassport.authenticate('jwt', { session: false }),
-};
-
-export const fastifyPreValidationRefreshToken = {
-  preValidation: fastifyPassport.authenticate('jwt-refresh-token', { session: false }),
-};
--- src/auth/interfaces.ts
+++
@@ -1,6 +0,0 @@
-import { JwtPayload as JwtPayloadExternal } from 'jsonwebtoken';
-
-export interface JwtPayload extends JwtPayloadExternal {
-  id: number;
-  uuid: string;
-}
