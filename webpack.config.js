2ade6788ac64db43a7392cc3c954e58d36d807be
Alex Seifert <alex@alexseifert.com>
2023-01-05 13:32:14 +0100

    Reconfigure Webpack to use subfolders

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   devtool: 'inline-source-map',
   entry: {
-    adminAsync: './assets/scripts/admin/async.ts',
-    adminImportant: './assets/scripts/admin/important.ts',
-    async: './assets/scripts/blog/async.ts',
-    important: './assets/scripts/blog/important.ts',
+    adminAsync: { import: './assets/scripts/admin/async.ts', filename: 'scripts/admin/async.js' },
+    adminImportant: { import: './assets/scripts/admin/important.ts', filename: 'scripts/admin/important.js' },
+    blogAsync: { import: './assets/scripts/blog/async.ts', filename: 'scripts/async.js' },
+    blogImportant: { import: './assets/scripts/blog/important.ts', filename: 'scripts/important.js' },
   },
   output: {
     filename: 'scripts/[name].js',
