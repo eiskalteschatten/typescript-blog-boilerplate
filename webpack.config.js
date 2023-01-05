ba139eb5835a9225d54f804f4d6b84cf63766066
Alex Seifert <alex@alexseifert.com>
2023-01-05 13:26:53 +0100

    Move the frontend scripts and SCSS to the assets folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   devtool: 'inline-source-map',
   entry: {
-    adminAsync: './src/frontend/admin/async.ts',
-    adminImportant: './src/frontend/admin/important.ts',
-    async: './src/frontend/blog/async.ts',
-    important: './src/frontend/blog/important.ts',
+    adminAsync: './assets/scripts/admin/async.ts',
+    adminImportant: './assets/scripts/admin/important.ts',
+    async: './assets/scripts/blog/async.ts',
+    important: './assets/scripts/blog/important.ts',
   },
   output: {
     filename: 'scripts/[name].js',
