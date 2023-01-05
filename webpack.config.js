3caa3ee1405b64ead7d86250361777d78b8f92ce
Alex Seifert <alex@alexseifert.com>
2023-01-05 09:25:52 +0100

    Start adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    admin: './src/frontend/admin/index.ts',
+    blog: './src/frontend/blog/index.ts',
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
