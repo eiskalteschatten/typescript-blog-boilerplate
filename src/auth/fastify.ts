8ccf5bed5945e9660b181adb5f2478937194b545
Alex Seifert <alex@alexseifert.com>
2023-06-04 16:59:27 +0200

    Move the login page url

--- src/auth/fastify.ts
+++ src/auth/fastify.ts
@@ -3,13 +3,13 @@ import fastifyPassport from '@fastify/passport';
 export const fastifyPreValidationLocal = {
   preValidation: fastifyPassport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/login',
+    failureRedirect: '/auth/login',
   }),
 };
 
 export const fastifyPreValidationLocalAdmin = {
   preValidation: fastifyPassport.authenticate('localAdmin', {
     successRedirect: '/admin',
-    failureRedirect: '/login',
+    failureRedirect: '/auth/login',
   }),
 };
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,10 +1,26 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 
-import { FastifyInstanceWithView } from '~/interfaces/fastify';
+import { FastifyInstanceWithView, FastifyReplyWithView } from '~/interfaces/fastify';
 
 export default async (app: FastifyInstanceWithView) => {
+  app.get('/', (req: FastifyRequest, reply: FastifyReply) => {
+    reply.redirect('/auth/login');
+  });
+
+  app.get('/login', (req: FastifyRequest, reply: FastifyReplyWithView) => {
+    if (req.isAuthenticated()) {
+      reply.redirect('/');
+      return;
+    }
+
+    reply.auth('login.ejs', {
+      title: 'Login',
+      error: null,
+    });
+  });
+
   app.post('/logout', (req: FastifyRequest, reply: FastifyReply) => {
     req.logout();
-    reply.redirect('/login');
+    reply.redirect('/auth/login');
   });
 };
