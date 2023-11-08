70929776b1924d8f96e6a333810e6418d690fb4d
Alex <alex@alexseifert.com>
2023-11-08 22:06:54 +0100

    Add the localAdmin Passport strategy

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -20,6 +20,24 @@ fastifyPassport.use('local', new LocalStrategy(async (username: string, password
   }
 }));
 
+fastifyPassport.use('localAdmin', new LocalStrategy(async (username: string, password: string, done: Function): Promise<void> => {
+  try {
+    const userService = new UserService();
+    const user = await userService.login(username, password);
+
+    // TODO: check role!
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  }
+  catch (error) {
+    done(error);
+  }
+}));
+
 fastifyPassport.registerUserSerializer(async (user: User) => user.id);
 
 fastifyPassport.registerUserDeserializer(async (id: number) => {
